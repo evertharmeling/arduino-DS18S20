896735a9bda35e67cba50d48918079eb065fa9ae
Evert Harmeling <evert.harmeling@freshheads.com>
2014-02-21 22:00:23 +0100

    Refactored code

--- arduinoDS18S20.ino
+++ arduinoDS18S20.ino
@@ -2,8 +2,14 @@
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
+#include <elapsedMillis.h>
 
-#define SENSOR_PIN  2
+#define PIN_SENSOR       2
+#define PIN_RELAIS_ONE   6
+#define PIN_RELAIS_TWO   7
+#define PIN_RELAIS_THREE 8
+#define PIN_RELAIS_FOUR  9
+
 #define SENSOR_HLT "10bab04c280b7"
 #define SENSOR_MLT "104bbc4d2805c"
 
@@ -16,13 +22,23 @@
 #define WHITE 0x7
 
 // on pin 10 (a 2.2K resistor is necessary)
-OneWire ds(SENSOR_PIN);
+OneWire ds(PIN_SENSOR);
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 String sensor;
 String mode;
+float tempHLT = 0;
+float tempMLT = 0;
+elapsedMillis elapsedTime;
 
+unsigned int interval = 1000;
+
+/**
+ *  Setup
+ */
 void setup() {
+    Serial.begin(9600);
+  
     // initialize lcd
     lcd.begin(16, 2);
     
@@ -32,12 +48,19 @@ void setup() {
     delay(1000);
     lcd.clear();
     
-    // show menu options, scrolled
+    // initialize Relais
+    pinMode(PIN_RELAIS_ONE, OUTPUT);
+    pinMode(PIN_RELAIS_TWO, OUTPUT);
+    pinMode(PIN_RELAIS_THREE, OUTPUT);
 }
 
+/**
+ *  Main loop
+ */
 void loop() {
     uint8_t buttons = lcd.readButtons();
 
+    // handle buttons
     if (buttons) {
         if (buttons & BUTTON_UP) {
             mode = "time";
@@ -49,42 +72,30 @@ void loop() {
         lcd.clear();
     }
     
+    float temp = readTemperature();
+    if (elapsedTime > interval) {
+
+        prepareRelais(temp);
+
+        // reset the counter to 0 so the counting starts over...
+	elapsedTime = 0;
+    }
+    
+    // handle button change
     if (mode == "time") {
         printTime();
     } else {
-        printTemperature();
+        printTemperature(temp);
     }
 }
 
-void printTime() {
-    int s = millis() / 1000;
-    int m = s / 60;
-    int h = m / 60;
-    lcd.setCursor(8,1);
-   
-    if (h < 10) {       // Add a zero, if necessary
-        lcd.print(0);
-    }
-   
-    lcd.print(h);
-    lcd.print(":");     // And print the colon
-   
-    if (m < 10) {       // Add a zero, if necessary, as above
-        lcd.print(0);
-    }
-   
-    lcd.print(m);
-    lcd.print(":");     // And print the colon
-   
-    s -= m * 60;
-    if (s < 10) {       // Add a zero, if necessary, as above
-        lcd.print(0);
-    }
-   
-    lcd.print(s);
-    delay(950);
-}
+/*
+ * ========================= FUNCTIONS =========================
+ */
 
+/**
+ *  Reads the temperature of the sensors, and stores these in `temp` variable and `sensor` variable
+ */
 float readTemperature() {
     byte i;
     byte data[12];
@@ -111,7 +122,7 @@ float readTemperature() {
     }
 
     ds.write(0x44, 1);  
-    delay(800);
+    //delay(800);
   
     ds.reset();
     ds.select(addr);    
@@ -130,10 +141,13 @@ float readTemperature() {
     return (float) raw / 16.0;
 }
 
-void printTemperature() {
-    float temp = readTemperature();
-  
-    if (temp) {
+/**
+ *  Prints the temperature to the LCD
+ *
+ *  @param float temp  The temperature in degrees Celsius
+ */
+void printTemperature(float temp) {
+    if (sensor && temp) {
         if (sensor == SENSOR_HLT) {
             lcd.setCursor(0, 0);
             lcd.print("HLT: ");
@@ -150,9 +164,121 @@ void printTemperature() {
         if (temp >= 100) {
             lcd.print(" ");
         } else {
-            lcd.print("  "); 
+            lcd.print("  ");
         }
         
         lcd.cursor();
+    } else if (elapsedTime > interval) {
+        lcd.setCursor(0, 0);
+        lcd.print("Geen sensoren");
+        lcd.setCursor(0, 1);
+        lcd.print("aangesloten... ");
+        lcd.cursor();
     }
+}
+
+/**
+ *  Handles the switching of the relais
+ *
+ *  @param float temp  The temperature in degrees Celsius
+ */
+void prepareRelais(float temp) {
+    if (sensor && temp) {
+        if (sensor == SENSOR_HLT) {
+            tempHLT = temp;
+        } else if (sensor == SENSOR_MLT) {
+            tempMLT = temp;
+        }
+      
+        // ensure we got the temperature of both sensor probes, so we can calculate a diff
+        if (sensor == SENSOR_HLT && tempHLT != 0 && tempMLT != 0) {
+            handleRelais(60, temp);
+        }
+    } else {
+        Serial.println("Geen sensoren aangesloten...");
+    }
+}
+
+/** 
+ *  Handles switching the relais, acting on temperature and temperature diff between HLT and MLT
+ *
+ *  @param float curTemp    Current temperature of the HLT sensor probe
+ *  @param float tempDiff   Difference in temperature between HLT and MLT
+ */
+void handleRelais(int setTemp, float curTemp) {
+    float tempDiff = setTemp - curTemp;
+    Serial.print("Current temp: ");
+    Serial.println(curTemp);
+    Serial.print("Temp diff: ");
+    Serial.println(tempDiff);
+  
+    if (curTemp < setTemp) {
+        Serial.println("Temp < " + String(setTemp));
+        
+        if (tempDiff > 1) {
+            switchRelais(PIN_RELAIS_ONE, true);
+        } else {
+            switchRelais(PIN_RELAIS_ONE, false);
+        }
+        if (tempDiff > 0.5) {
+            switchRelais(PIN_RELAIS_TWO, true);
+        } else {
+            switchRelais(PIN_RELAIS_TWO, false);
+        }
+        
+        switchRelais(PIN_RELAIS_THREE, true);
+    }
+    else {
+        Serial.println("else");
+        switchRelais(PIN_RELAIS_ONE, false);
+        switchRelais(PIN_RELAIS_TWO, false);
+        switchRelais(PIN_RELAIS_THREE, false);
+    }
+}
+
+/**
+ *  Switch relais on or off
+ *
+ *  @param int relais  The pin of the relais
+ *  @param boolean on  true / false
+ */
+void switchRelais(int relais, boolean on) {
+    if (on) {
+        Serial.println("Set relais " + String(relais) + " ON");
+        digitalWrite(relais, LOW);
+    } else {
+        Serial.println("Set relais " + String(relais) + " OFF");
+        digitalWrite(relais, HIGH);
+    }
+}
+
+/**
+ *  Prints the elapsed time
+ */
+void printTime() {
+    int s = millis() / 1000;
+    int m = s / 60;
+    int h = m / 60;
+    lcd.setCursor(8,1);
+   
+    if (h < 10) {       // Add a zero, if necessary
+        lcd.print(0);
+    }
+   
+    lcd.print(h);
+    lcd.print(":");     // And print the colon
+   
+    if (m < 10) {       // Add a zero, if necessary, as above
+        lcd.print(0);
+    }
+   
+    lcd.print(m);
+    lcd.print(":");     // And print the colon
+   
+    s -= m * 60;
+    if (s < 10) {       // Add a zero, if necessary, as above
+        lcd.print(0);
+    }
+   
+    lcd.print(s);
 }
