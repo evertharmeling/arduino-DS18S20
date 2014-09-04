74fda4e921da1783c6692216c1e6e1d4bb40b928
Evert Harmeling <evert.harmeling@freshheads.com>
2014-09-04 22:50:44 +0200

    Code cleanup

--- arduinoDS18S20.ino
+++ arduinoDS18S20.ino
@@ -1,3 +1,6 @@
+// @todo first maisch step may begin when the temp of the MLT is higher than first maisch step
+// @todo keep HLT on the last maisch temp always +/- 5 degrees warmer
+
 #include <OneWire.h>
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
@@ -15,9 +18,6 @@
 // Temperature probe addresses
 #define SENSOR_HLT "10bab04c280b7"
 #define SENSOR_MLT "104bbc4d2805c"
-//#define SENSOR_HLT "103d1325280a6"
-//#define SENSOR_MLT "1097e4242804d"
-
 #define SENSOR_BLT "103d1325280a6"
 #define SENSOR_EXT "1097e4242804d"
 
@@ -30,28 +30,37 @@
 #define TEAL    0x6
 #define WHITE   0x7
 
+// Define modes
+#define MODE_TIME     "time"
+#define MODE_HLT_MLT  "hlt_mlt"
+#define MODE_BLT_EXT  "blt_ext"
+
+#define DISPLAY_SERIAL "serial"
+#define DISPLAY_LCD    "lcd"
+
 // on pin 10 (a 2.2K resistor is necessary)
 OneWire ds(PIN_SENSOR_TEMPERATURE);
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
+// Initiate variables
 String sensor;
 String mode;
 elapsedMillis loopTime;
 elapsedMillis stepTime;
 
-unsigned int interval  = 1000;
+unsigned int interval  = 2000;
 float tempHLT          = 0;
 float tempMLT          = 0;
 float tempBLT          = 0;
 float tempEXT          = 0;
 boolean heatUpHLT      = true;
 boolean heatUpMLT      = true;
+float hysterese        = 0.5;
 
 boolean firstMaischPeriod  = false;
 int currentMaischStep      = 0;
 int maischTimes[]          = { 10, 45, 30, 1 };
 int maischTemperatures[]   = { 27, 62, 73, 78 };
-float lastTemp             = 78;
 
 /**
  *  Setup
@@ -75,6 +84,8 @@ void setup() {
     pinMode(PIN_RELAIS_TWO,   OUTPUT);
     pinMode(PIN_RELAIS_THREE, OUTPUT);
     pinMode(PIN_RELAIS_PUMP,  OUTPUT);
+    
+    mode = "hlt_mlt";
 }
 
 /**
@@ -86,10 +97,13 @@ void loop() {
     // handle buttons
     if (buttons) {
         if (buttons & BUTTON_UP) {
-            mode = "time";
+            mode = MODE_TIME;
         }
         if (buttons & BUTTON_LEFT) {
-            mode = "temp";
+            mode = MODE_HLT_MLT;
+        }
+        if (buttons & BUTTON_RIGHT) {
+            mode = MODE_BLT_EXT;
         }
         
         lcd.clear();
@@ -99,10 +113,10 @@ void loop() {
     prepareRelais(temp);
     
     // handle button change
-    if (mode == "time") {
+    if (mode == MODE_TIME) {
         lcdElapsedTime();
-    } else {
-        printTemperature(temp);
+    } else if (mode == MODE_HLT_MLT | mode == MODE_BLT_EXT) {
+        printTemperature(temp, mode);        
     }
 }
 
@@ -152,8 +166,10 @@ void prepareRelais(float temp) {
             Serial.println(currentMaischStep);
             Serial.print("Time elapsed: ");
             serialElapsedTime();
+            Serial.println("");
             Serial.print("Time elapsed current maisch step: ");
             serialStepTime();
+            Serial.println("");
           
             // set temperature for the maisch steps!
             handleRelais(tempHLT, tempMLT);
@@ -214,18 +230,29 @@ float readTemperature() {
 /**
  *  Prints the temperature to the LCD
  *
- *  @param float temp  The temperature in degrees Celsius
+ *  @param float temp   The temperature in degrees Celsius
+ *  @param String mode  The mode to print the right temperature value
  */
-void printTemperature(float temp) {
+void printTemperature(float temp, String mode) {
     if (sensor && temp) {
-        if (sensor == SENSOR_HLT) {
-            lcd.setCursor(0, 0);
-            lcd.print("HLT: ");
-        } else if (sensor == SENSOR_MLT) {
-            lcd.setCursor(0, 1);
-            lcd.print("MLT: ");
-        } 
-        
+        if (mode == MODE_HLT_MLT) {
+            if (sensor == SENSOR_HLT) {
+                lcd.setCursor(0, 0);
+                lcd.print("HLT: ");
+            } else if (sensor == SENSOR_MLT) {
+                lcd.setCursor(0, 1);
+                lcd.print("MLT: ");
+            } 
+        } else if (mode == MODE_BLT_EXT) {
+            if (sensor == SENSOR_BLT) {
+                lcd.setCursor(0, 0);
+                lcd.print("BLT: ");
+            } else if (sensor == SENSOR_EXT) {
+                lcd.setCursor(0, 1);
+                lcd.print("EXT: ");
+            }
+        }
+           
         lcd.print(temp);
         lcd.print(" ");
         lcd.print((char) 223);
@@ -238,18 +265,12 @@ void printTemperature(float temp) {
         }
         
         lcd.cursor();
-    } else if (loopTime > interval) {
-        lcd.setCursor(0, 0);
-        lcd.print("Geen sensoren");
-        lcd.setCursor(0, 1);
-        lcd.print("aangesloten... ");
-        lcd.cursor();
     }
 }
 
 /** 
  *  Handles switching the relais for heating up the HLT (by heaters) and MLT (by pumping wort through HLT spiral)
- *  The temperature of the HLT will be increased with 20 degrees, so heating up the MLT is quicker. The only catch
+ *  The temperature of the HLT will be increased with 30 degrees, so heating up the MLT is quicker. The only catch
  *  is to not heat up the HLT too much at the last maisch step (the water will be used to rinse the MLT)
  *
  *  @param float setTemp  The temperature to reach by the MLT
@@ -260,9 +281,8 @@ void handleRelais(float tempHLT, float tempMLT) {
   
     float setTemp = maischTemperatures[currentMaischStep];
   
-    float setTempHLT = setTemp + 20;
-    float setTempDiff = setTemp - tempMLT;
-    float hysterese = 0.5;
+    float setTempHLT = setTemp + 30;
+    float setTempDiff = tempMLT - setTemp;
 
     Serial.print("Current set temp HLT: ");
     Serial.println(setTempHLT + hysterese);
@@ -277,17 +297,17 @@ void handleRelais(float tempHLT, float tempMLT) {
     Serial.println("=================================");
      
     // Heat up the HLT
-    if (tempHLT > lastTemp) {
+    if (tempHLT > maischTemperatures[sizeof(maischTemperatures) - 1]) {
         heatUpHLT = false;
     } else {
-        heatUpHLT = handleHysterese(tempHLT, setTempHLT, heatUpHLT, hysterese);
+        heatUpHLT = handleHysterese(tempHLT, setTempHLT, heatUpHLT);
         switchRelais(PIN_RELAIS_ONE,   heatUpHLT);
         switchRelais(PIN_RELAIS_TWO,   heatUpHLT);
         switchRelais(PIN_RELAIS_THREE, heatUpHLT);
     }
     
     // According to the setTemp heat up the MLT by turning on the pump
-    heatUpMLT = handleHysterese(tempMLT, setTemp, heatUpMLT, hysterese);
+    heatUpMLT = handleHysterese(tempMLT, setTemp, heatUpMLT);
     switchRelais(PIN_RELAIS_PUMP, heatUpMLT);
 }
 
@@ -312,9 +332,9 @@ void switchRelais(int relais, boolean on) {
  *
  *  @param float temp       Current temperature value
  *  @param float setTemp    Desired temperature
- *  @param float hysterese  The amount added / substracted to the setTemp
+ *  @param boolean heating  Boolean to hold current heating status
  */
-boolean handleHysterese(float temp, float setTemp, boolean heating, float hysterese)
+boolean handleHysterese(float temp, float setTemp, boolean heating)
 {
     float top = setTemp + hysterese;
     float bottom = setTemp - hysterese;
@@ -341,55 +361,56 @@ void printTime(float time, String type) {
     int m = s / 60;
     int h = m / 60;
     
-    if (type == 'lcd') {
+    m -= h * 60;
+    s -= m * 60;
+    
+    if (type == DISPLAY_LCD) {
         lcd.setCursor(8,1);
     }
    
     if (h < 10) {       // Add a zero, if necessary
-        if (type == 'serial') {
+        if (type == DISPLAY_SERIAL) {
             Serial.print("0");
-        } else if (type == 'lcd') {
+        } else if (type == DISPLAY_LCD) {
             lcd.print(0);
         }   
     }
    
-    if (type == 'serial') {
+    if (type == DISPLAY_SERIAL) {
         Serial.print(h);
         Serial.print(":");     // And print the colon
-    } else if (type == 'lcd') {
+    } else if (type == DISPLAY_LCD) {
         lcd.print(h);
         lcd.print(":");     // And print the colon
     }
    
-    m -= h * 60;
     if (m < 10) {       // Add a zero, if necessary, as above
-        if (type == 'serial') {
+        if (type == DISPLAY_SERIAL) {
             Serial.print("0");
-        } else if (type == 'lcd') {
+        } else if (type == DISPLAY_LCD) {
             lcd.print(0);
         }
     }
    
-    if (type == 'serial') {
+    if (type == DISPLAY_SERIAL) {
         Serial.print(m);
         Serial.print(":");     // And print the colon
-    } else if (type == 'lcd') {
+    } else if (type == DISPLAY_LCD) {
         lcd.print(m);
         lcd.print(":");     // And print the colon
     }
    
-    s -= m * 60;
     if (s < 10) {       // Add a zero, if necessary, as above
-        if (type == 'serial') {
+        if (type == DISPLAY_SERIAL) {
             Serial.print("0");
-        } else if (type == 'lcd') {
+        } else if (type == DISPLAY_LCD) {
             lcd.print(0);
         }
     }
    
-    if (type == 'serial') {
+    if (type == DISPLAY_SERIAL) {
         Serial.print(s);
-    } else if (type == 'lcd') {
+    } else if (type == DISPLAY_LCD) {
         lcd.print(s);
     }
 }
@@ -398,19 +419,19 @@ void printTime(float time, String type) {
  *  Prints the elapsed time to LCD
  */
 void lcdElapsedTime() {
-    printTime(millis(), 'lcd');
+    printTime(millis(), DISPLAY_LCD);
 }
 
 /**
  *  Prints the elapsed time to Serial
  */
 void serialElapsedTime() {
-    serialTime(millis(), 'serial');
+    printTime(millis(), DISPLAY_SERIAL);
 }
 
 /**
  *  Prints the step time to Serial
  */
 void serialStepTime() {
-    serialTime(stepTime, 'serial');
+    printTime(stepTime, DISPLAY_SERIAL);
 }
